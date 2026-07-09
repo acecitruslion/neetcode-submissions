eec926224432d3854958792e5c557def1ee17a79
acecitruslion <121723446+acecitruslion@users.noreply.github.com>
2026-07-09 09:45:23 +0530

    Add: rotting-fruit - submission-1

---
+++ Data Structures & Algorithms/rotting-fruit/submission-1.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        int r = grid.size();
+        int c = grid[0].size();
+
+        queue<pair<int, int>> q;
+        vector<vector<int>> mins(r, vector<int>(c, 0));
+
+        for(int i=0;i<r;i++) {
+            for(int j=0;j<c;j++) {
+                if(grid[i][j] == 2) {
+                    q.push({i,j});
+                }
+            }
+        }
+        vector<int> dx = {-1, 1, 0, 0};
+        vector<int> dy = {0, 0, -1, 1};
+
+        while(!q.empty()) {
+            int row = q.front().first;
+            int col = q.front().second;
+            q.pop();
+
+            for(int i=0;i<4;i++) {
+                int nr = row + dx[i];
+                int nc = col + dy[i];
+
+                if(nr < 0 || nc < 0 || nr >= r || nc >= c || grid[nr][nc] != 1) {
+                    continue;
+                }
+
+                mins[nr][nc] = mins[row][col] + 1;
+                grid[nr][nc] = 0;
+                q.push({nr, nc});
+            }
+        }
+
+        int maxx = 0;
+
+        for(int i=0;i<r;i++) {
+            for(int j=0;j<c;j++) {
+                if(grid[i][j] == 1) return -1;
+                else if(grid[i][j] == 0 && mins[i][j] > maxx) {
+                    maxx = mins[i][j];
+                }
+            }
+        }
+        return maxx;
+    }
+};
